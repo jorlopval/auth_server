267f8faff8ed1634bf3656203fd121d998317c4e
Jorge LOPEZ VALERI <jorge.lopez@berger-levrault.com>
2022-12-09 23:23:18 +0100

    first commit

---
+++ models/Usuario.js
@@ -0,0 +1,20 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+
+const UsuarioSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports= mongoose.model('Usuario',UsuarioSchema);
